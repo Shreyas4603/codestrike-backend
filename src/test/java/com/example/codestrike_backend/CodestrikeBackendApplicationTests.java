ea80925705799619953879f05bdc368e1204885d
Shreyas <shreyasthegreat7@gmail.com>
2024-10-02 14:51:21 +0530

    first commit

---
+++ src/test/java/com/example/codestrike_backend/CodestrikeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codestrike_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodestrikeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
